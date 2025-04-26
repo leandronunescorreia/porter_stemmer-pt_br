082ef5904ef19d2326f8c02bff0d86ddf76a6d86
leandro correia <leandronunescorreia@outlook.com>
2025-04-26 15:26:14 -0300

    add a stop_words.json a little incomplete collection of pt-br stop-words
    add an example of small corpus

---
+++ examples/little_corpus.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from stemmer_pt_br import stemmer_pt_br
+import json
+from utils.string_utils import tokenize, create_word_mappings, encode_words, decode_indices
+
+stop_words = []
+with open("stop_words.json", "r", encoding="utf-8") as file:
+    stop_words = json.load(file)
+    stop_words = set(stop_words)
+
+sample_text = """
+Cantei, cantaremos, cantavam, cantando, cantaria, cantarão!
+Falava, falaram, falando, falaríamos.
+Vendemos, venderiam, venderão, vendendo.
+Corriam, correrei, correriam, correndo.
+Abriram, abrirei, abririam, abrindo.
+"""
+
+def main():
+    tokens = tokenize(sample_text, stop_words=stop_words)
+    stems = [stemmer_pt_br(word) for word in tokens]
+    unique_stems = sorted(set(stems))
+
+    word2idx, idx2word = create_word_mappings(unique_stems)
+    
+    corpus_encoded = encode_words(stems, word2idx)
+    corpus_decoded = decode_indices(corpus_encoded, idx2word)
+
+    print("Tokens:", tokens)
+    print("Stems:", stems)
+    print("Word2Idx:", word2idx)
+    print("Encoded corpus:", corpus_encoded)
+    print("Decoded back:", corpus_decoded)
+
+
+if __name__ == "__main__":
+    main()
--- utils/string_utils.py
+++ utils/string_utils.py
@@ -8,3 +8,20 @@ def remove_accents_and_punctuation(word):
     )
     word = ''.join(char for char in word if char not in string.punctuation)
     return word
+
+def tokenize(text, punctuation=string.punctuation, stop_words=None):
+    text = text.lower()
+    text = text.translate(str.maketrans('', '', punctuation))
+    tokens = text.split()
+    return [t for t in tokens if t not in stop_words]
+
+def create_word_mappings(unique_stems):
+    word2idx = {word: idx for idx, word in enumerate(unique_stems)}
+    idx2word = {idx: word for word, idx in word2idx.items()}
+    return word2idx, idx2word
+
+def encode_words(words, word2idx):
+    return [word2idx[word] for word in words]
+
+def decode_indices(indices, idx2word):
+    return [idx2word[idx] for idx in indices]
